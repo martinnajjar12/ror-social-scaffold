dd77f914b0ae430caa7e586922434230d587b0c3
Kingsley <kingobaino@gmail.com>
2020-12-09 16:33:09 +0100

    Fix some rubocop errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,11 +40,13 @@ class User < ApplicationRecord
     end
   end
 
+  # rubocop:disable Style/SoleNestedConditional, Lint/AssignmentInCondition
   def self.new_with_session(params, session)
     super.tap do |user|
-      if data = session["devise.facebook_data"] && session["devise.facebook_data"]["extra"]["raw_info"]
-        user.email = data["email"] if user.email.blank?
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
       end
     end
   end
+  # rubocop:enable Style/SoleNestedConditional, Lint/AssignmentInCondition
 end
